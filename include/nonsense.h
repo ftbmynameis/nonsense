8fca4adf042ee7fb8ea118073f4d45a8b4ba57f6
ftbmynameis <ftbmynameis@gmail.com>
2023-08-31 17:22:44 +0200

    add nonsense print function

---
+++ include/nonsense.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#if !defined(NONSENSE_EXPORT)
+#define NONSENSE_EXPORT
+#endif
+
+namespace nonsense
+{
+    NONSENSE_EXPORT void print_nonsense();
+}
---
+++ src/nonsense.cpp
@@ -0,0 +1,9 @@
+#include <nonsense.h>
+
+namespace nonsense
+{
+    void print_nonsense()
+    {
+        std::cout << "NONSENSE!" << std::endl;
+    }
+}
